af8b7bc6ea36b9a1a10d720daedb226d77926179
Mikhail <mi.sidoren@gmail.com>
2024-11-04 10:08:56 +0400

    Added new function for xlsx files downloading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from io import BytesIO
 from pathlib import Path
+import io
 
 from fastapi import HTTPException
 from googleapiclient.http import MediaIoBaseDownload
@@ -9,10 +10,10 @@ from gdrive.gdrive_service import get_drive_service
 project_root = Path(__file__).resolve().parent
 
 CREDENTIALS_FILE = f"{project_root}/credentials.json" # путь до файлика с кредами, который генерится в Google Cloud
-FILE_ID = "FILE ID ON G-DRIVE" # айди файла на гугл доке
+masterpaln_file_id = "" # id masterplan
 
 
-def download_file():
+def download_file(file_id):
     """"
         Функция для загрузки файлика .docx (это как пример) по его ID с Google Drive
     """
@@ -21,7 +22,7 @@ def download_file():
     try:
         # Создаем двоичный поток для скачивания
         file_stream = BytesIO()
-        request = drive_service.files().get_media(fileId=FILE_ID)
+        request = drive_service.files().get_media(fileId=file_id)
         downloader = MediaIoBaseDownload(file_stream, request)
 
         done = False
@@ -30,7 +31,7 @@ def download_file():
 
         # Перематываем на начало потока для чтения и получаем имя файла
         file_stream.seek(0)
-        file_info = drive_service.files().get(fileId=FILE_ID, fields="name, mimeType").execute()
+        file_info = drive_service.files().get(fileId=file_id, fields="name, mimeType").execute()
         file_name = file_info.get("name", "downloaded_file")
 
         # Проверяем, что файл ИМЕННО типа .docx
@@ -51,5 +52,29 @@ def download_file():
         raise HTTPException(status_code=500, detail=f"Ошибка при скачивании файла, {error}")
 
 
+def download_fileV2(file_id, file_name):
+    """
+    Скачивание файла с гугл драйв
+    :param file_id: ID файла, который необходимо скачать;
+    :param file_name: имя файла (С РАСШИРЕНИЕМ), которое будет использовано при сохранении файла в корень проекта
+    :return:
+    """
+    try:
+        request = get_drive_service(cred_file_path=CREDENTIALS_FILE).files().export_media(
+            fileId=file_id,
+            mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+
+        fh = io.FileIO(file_name, "wb")
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    print("FILE HAS BEEN DOWNLOADED")
+
 if __name__ == "__main__":
-    print(download_file())
+    download_fileV2(masterpaln_file_id, "test_excel.xlsx")
